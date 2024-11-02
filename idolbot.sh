f17522ac8153fa31767a826117081302beb365cd
Engielolz <109139281+Engielolz@users.noreply.github.com>
2024-11-02 12:01:14 -0700

    Implement pretty much everything except actually posting in idolbot

--- idolbot.sh
+++ idolbot.sh
@@ -25,7 +25,7 @@ function login () {
    if [ -z "$1" ] || [ -z "$2" ]; then $iecho "login params not specified"; return 1; fi
    getKeys $1 $2
    if ! [ "$?" = "0" ]; then $iecho "failed to log in"; return 1
-   else saveKeys ./$idol/secrets.env
+   else saveKeys ./data/$idol/secrets.env
    return 0
    fi
 }
@@ -38,7 +38,7 @@ function checkRefresh () {
          $iecho "Refresh error. Exiting."
          exit 1
       fi
-      saveSecrets $idol/secrets.env
+      saveSecrets data/$idol/secrets.env
    fi
 }
 
@@ -54,6 +54,12 @@ function repostLogic () {
       $iecho "Repost succeeded."
       return 0
    fi
+}
+
+function pickImage () {
+   imageNumber=$((1 + $RANDOM % $1 ))
+   echo $(echo "$images" | sed -n $imageNumber'p')
+   return
 }
 
 function postingLogic () {
@@ -75,26 +81,51 @@ function postingLogic () {
       event=regular
       ;;
    esac
-   if ! [ -f $idol/images/$event.txt ]; then event=regular; fi
+   if ! [ -f data/$idol/images/$event.txt ]; then event=regular; fi
    $iecho "Event: $event"
-   images=$(cat $idol/images/$event.txt)
+   images=$(grep -xvf data/$idol/recent.txt data/$idol/images/$event.txt)
+   if [ -z "$images" ]; then
+      $iecho "warning: all $event images in recents. falling back to regular"
+      images=$(grep -xvf data/$idol/recent.txt data/$idol/images/regular.txt)
+      if [ -z "$images" ]; then
+         $iecho "warning: all regular images used recently! ignoring recents.txt"
+         images=$(cat data/$idol/images/regular.txt)
+      fi
+   fi
+   image=$(pickImage $(echo "$images" | wc -l))
+   $iecho "picked image $image"
+   # very dirty loadSecrets call, not using it as intended
+   loadSecrets data/$idol/images/$image/info.txt
+   if ! [ "$?" = "0" ]; then
+      echo $iecho "fatal: the image data for $image is missing"
+      echo $iecho "hint: if the above line is broken, it may be encoded in windows format"
+      return 1
+   fi
+   $iecho "alt text: $alt"
+   if [ -z "$otheridols" ]; then
+      $iecho "image does not have other idols"
+   else
+      $iecho "image has other idols: $otheridols"
+   fi
+   # images=$(cat data/$idol/images/$event.txt)
    return 0
 }
 
-if ! [ -d ./$1 ]; then echo "idolbot: No such idol: $1"; exit 1; fi
+if ! [ -d ./data/$1 ]; then echo "idolbot: No such idol: $1"; exit 1; fi
 
 idol=$1
 iecho="echo $idol:"
-loadSecrets $idol/secrets.env
+loadSecrets data/$idol/secrets.env
 if ! [ "$?" = "0" ]; then
    if ! [ "$2" = "login" ]; then $iecho "you need to login first."; exit 1; fi
    login $3 $4
    exit $?
 fi
 if [ "$2" = "login" ]; then
-   if [ "$3" = "--force" ]; then login $3 $4
+   if [ "$3" = "--force" ]; then login $4 $5
    else $iecho "you are already logged in. Pass --force to log in anyway"; fi
 fi
 
-if [ "$2" = "post" ]; then postLogic; fi
+if [ "$2" = "post" ]; then postingLogic; fi
 if [ "$2" = "repost" ]; then repostLogic $3 $4; fi
+if [ -z "$2" ]; then $iecho "no operation specified"; exit 1; fi
